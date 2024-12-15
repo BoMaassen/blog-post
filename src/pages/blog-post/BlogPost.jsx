152d94d853812af17cf42e45c600b72357bb7f55
BoMaassen <bomaassen@live.nl>
2024-12-15 16:25:25 +0100

    2.2 af

--- src/pages/blog-post/BlogPost.jsx
+++ src/pages/blog-post/BlogPost.jsx
@@ -1,9 +1,23 @@
-import { useParams} from "react-router-dom";
+import posts from "../../constants/data.json";
+import {Link, useParams} from "react-router-dom";
+import Timestamp from "../../helpers/Timestamp.js";
 
-function BlogPost(){
+function BlogPost() {
     const {id} = useParams();
-    return(
-        <h1>Blog post {id}</h1>
+    return (
+        <main>
+            <div className="blog-post">
+                <h1>{posts[id - 1].title}</h1>
+                <h3>{posts[id].subtitle}</h3>
+                <p>Geschreven door {posts[id - 1].author} op {Timestamp(posts[id -1].created)} </p>
+                <p>{posts[id - 1].readTime} minuten lezen</p>
+                <p>{posts[id - 1].content}</p>
+                <p>{posts[id - 1].comments} reacties - {posts[id - 1].shares} keer gedeeld </p>
+            </div>
+            <p><Link to="/overview"> Terug naar de overzichtspagina</Link></p>
+        </main>
+
+
     )
 }
 
