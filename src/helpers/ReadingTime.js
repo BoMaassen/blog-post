ce22382dfb2a4450c7b4c6d5613a3d0ec89675cc
BoMaassen <bomaassen@live.nl>
2024-12-19 11:55:31 +0100

    opdracht 4 af

--- src/helpers/ReadingTime.js
+++ src/helpers/ReadingTime.js
@@ -1,6 +1,6 @@
 function ReadingTime(words){
     const amountOfWords = words.split(" ");
-    const time = Math.round(amountOfWords.length * 0.003) + " Min"
+    const time = Math.round(amountOfWords.length * 0.003)
     return time
 }
 export default ReadingTime
--- src/pages/new-post/NewPost.jsx
+++ src/pages/new-post/NewPost.jsx
@@ -1,72 +1,99 @@
 import {useForm} from "react-hook-form";
 import "./NewPost.css";
 import ReadingTime from "../../helpers/ReadingTime.js";
-import {useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
+import axios from "axios";
+import {useState} from "react";
 
 
-function NewPost(){
-    const {register, handleSubmit, formState:{errors}} = useForm();
-    const navigate = useNavigate();
+function NewPost() {
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const [error, toggleError] = useState(false);
+    const [succes, toggleSucces] = useState(false);
+    const [newBlogId, setNewBlogId] = useState(0);
 
-    function handelFormSubmit(data){
-        const timestamp = new Date().toISOString()
-
-        const newData = {
-            ...data,
-            "created": timestamp,
-            "comments": 0,
-            "shares": 0,
-            "readTime": ReadingTime(data.message)
+    async function handelFormSubmit(data) {
+        toggleError(false);
+        toggleSucces(false);
+        try {
+            const timestamp = new Date().toISOString()
+            const result = await axios.post("http://localhost:3000/posts",
+                {
+                    title: data.title,
+                    subtitle: data.subtitle,
+                    content: data.content,
+                    author: data.author,
+                    created: timestamp,
+                    readTime: ReadingTime(data.content),
+                    comments: 0,
+                    shares: 0
+                });
+            console.log(result.data);
+            toggleSucces(true);
+            setNewBlogId(result.data.id);
+        } catch (e) {
+            console.error(e);
+            toggleError(true);
         }
-        console.log(newData);
-        navigate("/overview")
     }
 
 
     return (
         <main className="new-post-container">
             <header>
-            <h1>Post toevoegen</h1>
+                <h1>Post toevoegen</h1>
             </header>
+            {succes ? <p>De blogpost is succesvol toegevoegd. Je kunt deze hier <Link
+                    to={`/blog-post/${newBlogId}`}> bekijken.</Link></p> :
+                <>
+                    {error && <h2 className="error-message">Er is iets misgegaan met het versturen. Probeer het
+                        opnieuw!</h2>}
 
-            <form onSubmit={handleSubmit(handelFormSubmit)} className="new-post-form">
-                <label htmlFor="title">
-                    Titel
+                    <form onSubmit={handleSubmit(handelFormSubmit)} className="new-post-form">
+                        <label htmlFor="title">
+                            Titel
 
-                    <input type="text" id="title" {...register("title", {
-                        required: { value:true, message:"Veld is verplicht"},})}/>
-                    {errors.title && <p>{errors.title.message}</p>}
-                </label>
-
-
-                <label htmlFor="title">
-                    Subtitel
-
-                    <input type="text" id="subtitle" {...register("subtitle", {
-                        required: { value:true, message:"Veld is verplicht"},})}/>
-                    {errors.subtitle && <p>{errors.subtitle.message}</p>}
-                </label>
-
-                <label htmlFor="title">
-                    Naam en achternaam
-
-                    <input type="text" id="author" {...register("author", {
-                        required:{ value:true, message:"Veld is verplicht"},})}/>
-                    {errors.author && <p>{errors.author.message}</p>}
-                </label>
-
-                <label htmlFor="message">
-                    Blogpost
-
-                    <textarea id="message" {...register("message", {
-                        required:{ value:true, message:"Veld is verplicht"}, minLength:{ value:300, message:"Minimaal 300 karakters"}, maxLength:{ value:2000, message:"Maximaal 2000 karakters"},})} rows="20" cols="5"></textarea>
-                    {errors.message && <p>{errors.message.message}</p>}
-                </label>
-
-                <button type="submit">Toevoegen</button>
+                            <input type="text" id="title" {...register("title", {
+                                required: {value: true, message: "Veld is verplicht"},
+                            })}/>
+                            {errors.title && <p>{errors.title.message}</p>}
+                        </label>
 
 
-            </form>
+                        <label htmlFor="title">
+                            Subtitel
+
+                            <input type="text" id="subtitle" {...register("subtitle", {
+                                required: {value: true, message: "Veld is verplicht"},
+                            })}/>
+                            {errors.subtitle && <p>{errors.subtitle.message}</p>}
+                        </label>
+
+                        <label htmlFor="title">
+                            Naam en achternaam
+
+                            <input type="text" id="author" {...register("author", {
+                                required: {value: true, message: "Veld is verplicht"},
+                            })}/>
+                            {errors.author && <p>{errors.author.message}</p>}
+                        </label>
+
+                        <label htmlFor="message">
+                            Blogpost
+
+                            <textarea id="content" {...register("content", {
+                                required: {value: true, message: "Veld is verplicht"},
+                                minLength: {value: 300, message: "Minimaal 300 karakters"},
+                                maxLength: {value: 2000, message: "Maximaal 2000 karakters"},
+                            })} rows="20" cols="5"></textarea>
+                            {errors.content && <p>{errors.content.message}</p>}
+                        </label>
+
+                        <button type="submit">Toevoegen</button>
+
+
+                    </form>
+                </> }
         </main>
     )
 }
